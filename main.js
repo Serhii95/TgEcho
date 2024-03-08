37275b0e8da20aa0d1e16c78dfc482bb1a416dbe
Serhii95 <103376178+Serhii95@users.noreply.github.com>
2024-03-08 20:25:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7031361604:AAE6kwMBBBC6qvyAqqLRzfbOJ07KlD0Y7tQ';
+const id = 616100619;
+const urlPhoto = 'https://picsum.photos/200/300';
+const bot = new TelegramBot(token, { polling: true })
+console.log('Telegram bot succesfully started...\n');
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    if (msg.text.toLowerCase() === 'photo') {
+        console.log(`Користувач ${msg.from.first_name} зробив запит фото!`);
+        getPhoto().then((response) => sendPhotoMessage(chatId, response.data));
+    } else {
+        console.log(`Користувач ${msg.from.first_name} надіслав: ${msg.text}!`);
+    }
+})
+
+
+function sendPhotoMessage(chatId, stream) {
+    bot.sendPhoto(chatId, stream, { filename: 'filename.jpg', contentType: 'image/jpeg' });
+}
+
+function getPhoto() {
+    return axios({
+        url: urlPhoto,
+        method: 'GET',
+        responseType: 'stream',
+    });
+}
